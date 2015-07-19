c855488cbeef45af69b39374157b0f6aae0bf22e
esc <esc@youhavethewrong.info>
2015-07-18 21:55:05 -0500

    License and update to 1.7

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject v "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
   		 [uncomplicate/clojurecl "0.1.2"]]
   :main ^:skip-aot v.core
   :target-path "target/%s"
