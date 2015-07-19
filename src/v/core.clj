13d658586661623326aab64859fc2b17fa5a76a9
esc <esc@youhavethewrong.info>
2015-07-18 21:44:34 -0500

    Dump device info

--- src/v/core.clj
+++ src/v/core.clj
@@ -1,11 +1,11 @@
 (ns v.core
-  (:require [uncomplicate.clojurecl.core :as core]
-            [uncomplicate.clojurecl.info :as info])
+  (:require
+   [clojure.pprint :refer [pprint]]
+   [uncomplicate.clojurecl.core :as core]
+   [uncomplicate.clojurecl.info :as info])
   (:gen-class :main true))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
-  (println (info/info (core/platforms)))
-  (println (info/info (core/devices (first (core/platforms))))))
+  (pprint (map info/info (core/devices (first (core/platforms))) )) ) 
